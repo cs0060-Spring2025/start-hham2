c7cdd42c323627f994395621f1a07834bf4184f7
hham2 <86435642+hham2@users.noreply.github.com>
2025-01-22 16:02:42 -0500

    Update start.py

--- start.py
+++ start.py
@@ -7,5 +7,5 @@ Test Program
 # Once done, commit the changes
 
 
-print ("Hi there, my name is David Prager")
-print ("My GitHub username is -------")
+print ("Hi there, my name is Henrik Hamilton")
+print ("My GitHub username is hham2")
